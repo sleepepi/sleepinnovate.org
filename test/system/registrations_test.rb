fb526622128c3c85621950e75646f66254b76d9a
Remo Mueller <remosm@gmail.com>
2017-09-14 17:00:21 -0400

    Renamed tests

--- test/system/registrations_test.rb
+++ test/system/registrations_test.rb
@@ -6,63 +6,63 @@ require "application_system_test_case"
 class RegistrationsTest < ApplicationSystemTestCase
   test "registering and consenting" do
     visit root_url
-    screenshot("register-consent")
+    screenshot("consent-register-profile")
     click_on "get started"
-    screenshot("register-consent")
+    screenshot("consent-register-profile")
     click_on "Read consent"
-    screenshot("register-consent")
+    screenshot("consent-register-profile")
     assert_selector "h1", text: "Consent"
     page.execute_script("$(\"#read_consent\").click();")
     page.execute_script("window.scrollBy(0, $(\"body\").height());")
-    screenshot("register-consent")
+    screenshot("consent-register-profile")
     click_on "I Consent"
     assert_selector "div", text: "Create your account"
     fill_in "user[full_name]", with: "John Smith"
     fill_in "user[email]", with: "jsmith@example.com"
     fill_in "user[email_confirmation]", with: "jsmith@example.com"
-    screenshot("register-consent")
+    screenshot("consent-register-profile")
     click_on "Create Account"
     assert_equal "John Smith", User.last.full_name
     assert_equal "jsmith@example.com", User.last.email
-    screenshot("register-consent")
+    screenshot("consent-register-profile")
     assert_equal true, User.last.consented?
     assert_selector "h1", text: "Complete Your Profile"
     select "Dec", from: "date_of_birth[month]"
     select "31", from: "date_of_birth[day]"
     select "1984", from: "date_of_birth[year]"
     fill_in "address", with: "123 Road Street\nCity, ST 12345"
-    screenshot("register-consent")
+    screenshot("consent-register-profile")
     click_on "Confirm identity"
     assert_equal "1984-12-31", User.last.date_of_birth
     assert_equal "123 Road Street City, ST 12345", User.last.address
     sleep(0.5) # Allow time to setup canvas for drawing
     draw_on_canvas
-    screenshot("register-consent")
+    screenshot("consent-register-profile")
     click_on "Finish enrollment"
     assert_selector "a", text: "Start survey"
-    screenshot("register-consent")
+    screenshot("consent-register-profile")
   end
 
-  test "registering and refusing" do
+  test "read consent and refuse" do
     visit root_url
-    screenshot("register-refuse")
+    screenshot("refuse-consent")
     click_on "get started"
-    screenshot("register-refuse")
+    screenshot("refuse-consent")
     click_on "Read consent"
-    screenshot("register-refuse")
+    screenshot("refuse-consent")
     page.execute_script("window.scrollBy(0, $(\"body\").height());")
-    screenshot("register-refuse")
+    screenshot("refuse-consent")
     page.accept_confirm "Click \"OK\" to exit enrollment process." do
       click_on "I Do Not Consent"
     end
     assert_selector "p", text: "Thank you for considering to participate"
-    screenshot("register-refuse")
+    screenshot("refuse-consent")
     click_on "Start new session"
     assert_selector "h1", text: "SleepINNOVATE"
-    screenshot("register-refuse")
+    screenshot("refuse-consent")
   end
 
-  test "registering and skipping consent" do
+  test "register without consenting" do
     visit root_url
     screenshot("register-skip-consent")
     click_on "get started"
@@ -82,11 +82,11 @@ class RegistrationsTest < ApplicationSystemTestCase
     screenshot("register-skip-consent")
   end
 
-  test "registering and skipping profile" do
+  test "read consent register and skip profile" do
     visit root_url
-    screenshot("register-skip-profile")
+    screenshot("consent-register-skip-profile")
     click_on "get started"
-    screenshot("register-skip-profile")
+    screenshot("consent-register-skip-profile")
     click_on "Read consent"
     assert_selector "h1", text: "Consent"
     page.execute_script("$(\"#read_consent\").click();")
@@ -95,16 +95,16 @@ class RegistrationsTest < ApplicationSystemTestCase
     fill_in "user[full_name]", with: "John Smith"
     fill_in "user[email]", with: "jsmith@example.com"
     fill_in "user[email_confirmation]", with: "jsmith@example.com"
-    screenshot("register-skip-profile")
+    screenshot("consent-register-skip-profile")
     click_on "Create Account"
     assert_equal "John Smith", User.last.full_name
     assert_equal "jsmith@example.com", User.last.email
-    screenshot("register-skip-profile")
+    screenshot("consent-register-skip-profile")
     assert_equal true, User.last.consented?
     visit dashboard_url
     assert_equal false, User.last.profile_complete?
     assert_selector "h2", text: "Complete your profile"
-    screenshot("register-skip-profile")
+    screenshot("consent-register-skip-profile")
   end
 
   # data = window.$signaturePad.toData()
