965f3e07a864f2f28ae296a82067e7d72fc4496e
Remo Mueller <remosm@gmail.com>
2018-02-05 09:53:52 -0500

    Version bump to 6.0.0.pre

--- config/initializers/version.rb
+++ config/initializers/version.rb
@@ -2,10 +2,10 @@
 
 module SleepInnovate
   module VERSION #:nodoc:
-    MAJOR = 5
+    MAJOR = 6
     MINOR = 0
-    TINY = 1
-    BUILD = nil # "pre", "beta1", "beta2", "rc", "rc2", nil
+    TINY = 0
+    BUILD = "pre" # "pre", "beta1", "beta2", "rc", "rc2", nil
 
     STRING = [MAJOR, MINOR, TINY, BUILD].compact.join(".")
   end
