c7c6b54d9c8d6eb0dc84ed2714aaac76a5e0715b
Remo Mueller <remosm@gmail.com>
2018-02-05 10:29:48 -0500

    Updated to bootstrap 4.0.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,6 +4,7 @@
 - **Gem Changes**
   - Updated to rails 5.2.0.rc1
   - Updated to pg 1.0.0
+  - Updated to bootstrap 4.0.0
 
 ## 5.0.1 (January 16, 2018)
 
--- gems.rb
+++ gems.rb
@@ -9,7 +9,7 @@ gem "pg",                   "1.0.0"
 
 # Gems used by project
 gem "autoprefixer-rails"
-gem "bootstrap",            "~> 4.0.0.beta3"
+gem "bootstrap",            "~> 4.0.0"
 gem "carrierwave",          "~> 1.2.1"
 gem "colorize",             "~> 0.8.1"
 gem "devise",               "~> 4.4.0"
