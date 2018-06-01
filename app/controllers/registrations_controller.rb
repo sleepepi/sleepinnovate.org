147137d9ebcd6a268c96c13af5a17d51e5e8a6af
Remo Mueller <remosm@gmail.com>
2018-06-01 10:13:41 -0400

    Removed recaptcha

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,8 +1,7 @@
 # frozen_string_literal: true
 
-# Adds a recaptcha on registration.
+# Generates password and welcome email on registration.
 class RegistrationsController < Devise::RegistrationsController
-  prepend_before_action :check_captcha, only: [:create]
   prepend_before_action :generate_password, only: [:create]
   append_after_action :generate_welcome_email, only: [:create]
 
@@ -20,32 +19,6 @@ class RegistrationsController < Devise::RegistrationsController
 
   def add_from_session(resource, key)
     resource.update key => session[key] if session[key].present?
-  end
-
-  def check_captcha
-    return unless RECAPTCHA_ENABLED && !verify_recaptcha
-    self.resource = resource_class.new sign_up_params
-    resource.errors.add(:recaptcha, "reCAPTCHA verification failed.")
-    respond_with_navigational(resource) { render :new }
-  end
-
-  def verify_recaptcha
-    url = URI.parse("https://www.google.com/recaptcha/api/siteverify")
-    http = Net::HTTP.new(url.host, url.port)
-    if url.scheme == "https"
-      http.use_ssl = true
-      http.verify_mode = OpenSSL::SSL::VERIFY_NONE
-    end
-    post_params = [
-      "secret=#{ENV["recaptcha_secret_key"]}",
-      "response=#{params["g-recaptcha-response"]}",
-      "remoteip=#{request.remote_ip}"
-    ]
-    response = http.start do |h|
-      h.post(url.path, post_params.join("&"))
-    end
-    json = JSON.parse(response.body)
-    json["success"]
   end
 
   def generate_password
--- config/initializers/recaptcha.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-RECAPTCHA_ENABLED = \
-  if Rails.env.test?
-    false
-  else
-    ENV["recaptcha_enabled"] == "true"
-  end
