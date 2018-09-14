d67065f63700125197829d91bd7f58e7338fe099
Remo Mueller <remosm@gmail.com>
2018-09-14 12:07:12 -0400

    Fix system test

--- test/system/internal_test.rb
+++ test/system/internal_test.rb
@@ -8,12 +8,12 @@ class InternalTest < ApplicationSystemTestCase
     @consented_user = users(:consented)
   end
 
-  test "visiting the dashboard" do
+  test "visit the dashboard" do
     password = "PASSword2"
     @consented_user.update(password: password, password_confirmation: password)
     complete_profile!(@consented_user)
     visit root_url
-    click_on "Sign in"
+    click_on "Sign in", match: :first
     screenshot("visiting-dashboard")
     fill_in "user[email]", with: @consented_user.email
     fill_in "user[password]", with: @consented_user.password
